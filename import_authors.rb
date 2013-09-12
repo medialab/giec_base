621dcbcadad7a4f100d4ae31d10d431e8fdfaeaf
Yomguithereal <guillaumeplique@gmail.com>
2013-09-12 12:36:03 +0200

    Fix for international organizations

--- import_authors.rb
+++ import_authors.rb
@@ -53,7 +53,7 @@ class Importer
 		CSV.foreach("feed/Authors.csv", {:headers => :first_row}) do |row|
 
 			# Users
-			aut = {:id => row[0], :first_name => row[1], :last_name => row[2], :groupings => row[16]}
+			aut = {:id => row[0], :first_name => row[1], :last_name => row[2]}
 			ins = {:name => row[8], :type => row[7], :department => row[9], :country => row[10]}
 
 			# Yielding rows
@@ -68,16 +68,18 @@ class Importer
 		load_csv do |ins, aut|
 
 			# Registering institution
+			if ins[:type] == "INT" then ins_key = ins[:name]+'||'+ins[:country] else ins_key = ins[:name] end
+
 			if ins[:name] != nil
 				if ins[:name] != "N/A" and ins[:name] != ''
-					if !@institutions.has_key?(ins[:name])
+					if !@institutions.has_key?(ins_key)
 						model = Institution.new(:name => ins[:name], :country_id => @countries[ins[:country]])
-						@institutions.store(ins[:name], model)
+						@institutions.store(ins_key, model)
 					else
 
 						# Registering country if we find it elsewhere
-						if @institutions[ins[:name]].country == nil and ins[:country] != nil
-							@institutions[ins[:name]][:country_id] = @countries[ins[:country]]
+						if @institutions[ins_key].country == nil and ins[:country] != nil
+							@institutions[ins_key][:country_id] = @countries[ins[:country]]
 						end
 					end
 
@@ -89,15 +91,15 @@ class Importer
 							model = Department.new(:name => ins[:department])
 							
 							# Linking to institution
-							@institutions[ins[:name]].departments << model
+							@institutions[ins_key].departments << model
 							@departments.store(depkey, model)
 						end
 					end
 
 					# Registering Institution Types
 					if ins[:type] != nil and ins[:type] != ''
-						if @institutions[ins[:name]].type == nil
-							@institutions[ins[:name]].type = @taxonomy[ins[:type]]
+						if @institutions[ins_key].type == nil
+							@institutions[ins_key].type = @taxonomy[ins[:type]]
 						end
 					end
 				end
@@ -114,7 +116,7 @@ class Importer
 			if ins[:name] != '' and ins[:name] != 'N/A' and ins[:name] != nil
 				if !@registers[aut[:id]].include?(ins[:name])
 					@registers[aut[:id]].push(ins[:name])
-					@authors[aut[:id]].institutions << @institutions[ins[:name]]
+					@authors[aut[:id]].institutions << @institutions[ins_key]
 
 					if depkey != nil
 						@authors[aut[:id]].departments << @departments[depkey]
