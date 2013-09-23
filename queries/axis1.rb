00eef3ce6aa288de70f0ff7696b44ed5d57a48ce
Yomguithereal <guillaumeplique@gmail.com>
2013-09-23 17:54:41 +0200

    Daily backup

---
+++ queries/axis1.rb
@@ -0,0 +1,70 @@
+# -------------------------------------------------------------------
+# Participations Query
+# -------------------------------------------------------------------
+#
+#
+#   Author : PLIQUE Guillaume
+#   Organization : Medialab - Sciences Po
+#   Version : 1.0
+
+require_relative "subqueries.rb"
+require_relative "../model/taxonomies.rb"
+require_relative "../tools/mixins.rb"
+
+class Query
+    include Helpers::Export
+
+    # Base properties definition
+    def initialize
+
+        # Taxonomies
+        @geo_groups = Taxonomies::Groups.select {|g| g[:type] == "GEO"}
+        @geo_symbols = @geo_groups.map {|g| g[:symbol]}
+
+        # Headers
+        @vizA_header = ["population"] + @geo_groups.map {|g| g[:name]}
+    end
+
+    # Query Execution
+    def exec
+
+        # Subparts
+        # viz('vizA', CrossARAuthors.get, CrossARAuthors.getByRoles)
+        # viz('vizB', CrossARAuthors.get(1), CrossARAuthors.getByRoles(1))
+
+        return @export
+    end
+
+    # Viz A & B
+    def viz(name, total_population, roled_population)
+        csv = [@vizA_header]
+
+        for i in 1..5
+            csv.push get_row("total population for min AR count #{i}", total_population, i)
+            csv.push get_row("total population for min AR count #{i} (LA, LCA, RE)", roled_population, i)
+        end
+
+        addToExport({:name => name, :data => csv})
+    end
+
+    def get_row(description, population, ar_count)
+        group_counts = {}
+        @geo_groups.map {|g| group_counts.store(g[:symbol], 0)}
+
+        for a in population.select {|a| a._data[:ars].length >= ar_count}
+            if a.institutions.length > 0
+                if a.institutions[-1].country != nil
+                    for gp in a.institutions[-1].country.groupings
+                        if @geo_symbols.include?(gp.symbol)
+                            group_counts[gp.symbol] += 1
+                        end
+                    end
+                end
+            end
+        end
+
+        return [description] + group_counts.values
+    end
+
+    # for each gp get ins then get distinct authors with ins IN (result)
+end
