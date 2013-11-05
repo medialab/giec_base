d2c5dbb39c8504b000329d429d46bbf0087f7079
Yomguithereal <guillaumeplique@gmail.com>
2013-11-05 18:03:26 +0100

    Ianzilla 2 backup

--- queries/ianzilla.rb
+++ queries/ianzilla.rb
@@ -17,16 +17,23 @@ class Query
     # Base properties definition
     def initialize
 
+        # Model
+        @themes = ChapterType.all
+        @countries = Country.all
+
         # Headers
         @header = ["id", "first_name", "last_name", "participations (AR.WG)", "WGS count", "last_institution", "pays", "LCA", "LA", "RE", "CA"]
+        @header2 = ["country"] + @themes.map {|t| t.name}    
     end
 
     # Query Execution
     def exec
 
         # Subparts
-        addToExport({:data => ianzilla, :name => "ianzilla"})
+        # addToExport({:data => ianzilla, :name => "ianzilla"})
         # addToExport({:data => everyone, :name => "everyone"})
+        addToExport({:data => ianzilla2, :name => "ianzilla2"})
+        ianzilla2
         return @export
     end
 
@@ -81,6 +88,27 @@ class Query
                 ca = (Participation.first(:author_id => a.id, :role => 'CA') != nil) ? 'yes' : 'no',
             ]
 
+            csv.push rowgmail
+
+        end
+
+        return csv
+    end
+
+    # Query 2
+    def ianzilla2
+        csv = [@header2]
+
+        for c in @countries
+            row = [c.name]
+            for t in @themes
+                chs = t.chapters
+                pl = 0
+                for ch in t.chapters
+                    pl += Participation.all('institution.country.name' => c.name, :ar => ch.ar, :wg => ch.wg, :chapter => ch.number).length
+                end
+                row.push pl
+            end
             csv.push row
         end
 
